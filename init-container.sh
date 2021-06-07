3bdb2e5768a2445a3457ac0b910f87d9da946c99
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-06-07 16:49:01 +0200

    jlink: only mention sub-GHz domains
    "WW" (Word Wide) use 2.4Ghz. However, none the devices Silabs supply are
    able to use this band.

--- init-container.sh
+++ init-container.sh
@@ -40,8 +40,8 @@ Container options:
 
 Wi-SUN options:
   -n, --ws-network=NAME Set Wi-SUN network name.
-  -C, --ws-domain=CC    Set Wi-SUN regulatory domain. Valid values: WW, EU, NA,
-                        JP, ...).
+  -C, --ws-domain=CC    Set Wi-SUN regulatory domain. Valid values: EU, NA, JP,
+                        ...).
   -m, --ws-mode=HEX     Set operating mode. Valid values: 1a, 1b, 2a, 2b, 3, 4a,
                         4b and 5.
   -c, --ws-class=NUM    Set operating class. Valid values: 1, 2 or 3.
